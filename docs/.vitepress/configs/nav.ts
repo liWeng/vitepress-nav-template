c8b864f7f750649bb7fb3f0a2c885f6afa0e230a
liwen <liwen@ieforever.com>
2023-10-19 17:32:40 +0800

    修改目录结构及文档描述

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -6,8 +6,8 @@ export const nav: DefaultTheme.Config['nav'] = [
     items: [
       {
         items: [
-          { text: "summer-Vue组件库", link: "/markdown/main-summer.md" },
-          { text: "market-Vue组件库", link: "/markdown/main-market.md" },
+          { text: "summer-Vue组件库", link: "/articles/assembly/main-summer.md" },
+          { text: "market-Vue组件库", link: "/articles/assembly/main-market.md" },
         ],
       },
       {
@@ -20,10 +20,43 @@ export const nav: DefaultTheme.Config['nav'] = [
   },
   {
     text: '后端',
-    link: 'https://notes.fe-mm.com'
+    link: ''
   },
-  { text: '规范', link: 'https://netease-music.fe-mm.com' },
-  { text: '部署', link: 'https://netease-music.fe-mm.com' },
+  { text: '规范',
+    items: [
+      {
+        items: [
+          { text: "前端规范", link: "/articles/norm/开发规范-前端.md" },
+          { text: "静态资源引用", link: "/articles/norm/开发规范-静态资源引用.md" },
+        ],
+      },
+      {
+        items: [
+          { text: "开发规范-数据库", link: "/articles/norm/开发规范-数据库.md" },
+          { text: "MYSQL-执行计划", link: "/articles/norm/MYSQL-SQL执行计划详解.md" },
+          { text: "MYSQL-数据库优化", link: "/articles/norm/MYSQL-数据库优化.md" },
+        ],
+      },
+      {
+        items: [
+          { text: "开发规范-工程及依赖", link: "/articles/norm/开发规范-工程及依赖.md" },
+          { text: "开发规范-java", link: "/articles/norm/开发规范-java.md" },
+          { text: "代码管理规范", link: "/articles/norm/代码管理规范.md" },
+        ],
+      },
+      {
+        items: [
+          { text: "工程创建说明", link: "/articles/norm/工程创建说明.md" },
+          { text: "更新日志规范格式", link: "/articles/norm/更新日志规范格式.md" },
+        ],
+      },
+    ],
+  },
+  { text: '部署',
+    items: [
+
+    ]
+  },
   {
     text: '日常',
     link: 'https://github.com/maomao1996/tampermonkey-scripts'
--- docs/.vitepress/configs/sidebar.ts
+++ docs/.vitepress/configs/sidebar.ts
@@ -1,29 +1,60 @@
 import type { DefaultTheme } from 'vitepress'
 
 export const sidebar: DefaultTheme.Config['sidebar'] = {
-  "/markdown/": [
+  "/articles/assembly/": [
     {
       text: "前端组件库",
-      collapsed:true,
+      collapsed:false,
       items: [
         {
           text: "summer-Vue组件库",
-          link: "/markdown/main-summer.md",
+          link: "/assembly/main-summer.md",
         },
         {
           text: "market-Vue组件库",
-          link: "/markdown/main-market.md",
+          link: "/assembly/main-market.md",
         }
       ],
     },
     {
       text: "vue教程",
+      collapsed:false,
       items: [
         {
           text: "pina和vuex",
-          link: "/markdown/pina和vuex",
+          link: "/assembly/pina和vuex",
         },
       ],
     },
+  ],
+  "/articles/norm/": [
+    {
+      text: '前端',
+      collapsed:false,
+      items: [
+        { text: "前端规范", link: "/articles/norm/开发规范-前端.md" },
+        { text: "静态资源引用", link: "/articles/norm/开发规范-静态资源引用.md" }
+      ],
+    },
+    {
+      text: '开发规范',
+      collapsed:false,
+      items: [
+        { text: "开发规范-数据库", link: "/articles/norm/开发规范-数据库.md" },
+        { text: "开发规范-工程及依赖", link: "/articles/norm/开发规范-工程及依赖.md" },
+        { text: "开发规范-java", link: "/articles/norm/开发规范-java.md" },
+        { text: "代码管理规范", link: "/articles/norm/代码管理规范.md" },
+        { text: "工程创建说明", link: "/articles/norm/工程创建说明.md" },
+        { text: "更新日志规范格式", link: "/articles/norm/更新日志规范格式.md" },
+      ],
+    },
+    {
+      text: 'MYSQL数据库',
+      collapsed:false,
+      items: [
+        { text: "MYSQL-执行计划", link: "/articles/norm/MYSQL-SQL执行计划详解.md" },
+        { text: "MYSQL-数据库优化", link: "/articles/norm/MYSQL-数据库优化.md" },
+      ],
+    }
   ],
 }
